84263e6992d197a2d96ac6b15d2f71d395075e4b
jumaidin12 <jumaidinasa@gmail.com>
2021-09-03 19:26:40 +0700

    menambahkan mockup

---
+++ fr.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Selamat Datang Di Pendaftaran Pesantren</title>
+</head>
+
+<!-- link css -->
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+<body>
+  <!-- header -->
+  <div class="container">
+    <div class="wrap">
+      <img src="b.jpeg" width="1110px" height="180px"></div><br/>
+
+      <div class="wrap" style="background-color:white;">
+       <div class="wrap " style="background-color: grey" align="center">Informasi Sekolah</div>
+
+       <!-- table -->
+       <table width="750">
+        <tr>
+          <td>No Pendaftaran</td>
+          <td><input type="text"></td>
+        </tr>
+
+        <tr>
+          <td>Nama</td>
+          <td><input type="text" name=""></td>
+        </tr>
+
+        <tr>
+          <td>Tempat Tanggal Lahir</td>
+          <td><input type="text" name=""></td>
+          <td><input type="Date" name=""></td>
+        </tr>
+
+        <tr>
+          <td>Alamat</td>
+          <td><textarea cols="22" rows="3" ></textarea></td>
+        </tr>
+
+        <tr>
+          <td>Agama</td>
+          <td>
+            <select>
+              <option></option>
+              <option>Islam</option>
+              <option>Kristen</option>
+              <option>Kotolik</option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>Jenis Kelamin</td>
+          <td><input type="radio" name='jenis_kelamin' value='pria'/>Pria
+            <input type="radio" name='jenis_kelamin' value='perempuan'/>Perempuan</td>
+          </tr>
+
+          <tr>
+            <td>Status</td>
+            <td><select>
+              <option></option>
+              <option>Menikah</option>
+              <option>Belum Menikah</option>
+            </select>
+          </tr>
+
+          <tr>
+            <td>Kewarganegaraan</td>
+            <td>
+             <select>
+              <option></option>
+              <option>Indonseia</option>
+              <option>WNA</option>
+            </select>
+          </td>
+
+          <tr>
+            <td>Pekerjaan</td>
+            <td><input type="text" name=""></td>
+          </tr>
+
+          <tr>
+            <td>Kelurahan</td>
+            <td><input type="text" name=""></td>
+
+          </tr>
+          <tr>
+            <td>Kecamatan</td>
+            <td><input type="text" name=""></td>
+          </tr>
+
+          <tr>
+            <td>Kota/Kabupaten</td>
+            <td><input type="text" name=""></td>
+          </tr>
+
+          <tr>
+            <td>Provinsi</td>
+            <td><select>
+              <option>Pilih Provinsi</option>
+              <option value=""></option>
+              <option value="Jawa Barat"></option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>No Telpon</td>
+          <td><input type="text" name=""></td>
+        </tr>
+      </table>
+
+      <div class="wrap" style="background-color: grey" align="center">Informasi Sekolah</div>
+      <table width="650">
+        <tr>
+          <td>Nama Sekolah</td>
+          <td><input type="text" name=""></td>
+        </tr>
+
+        <tr>
+          <td>Asal Sekolah</td>
+          <td><input type="text" name=""></td>
+        </tr>
+        <tr>
+          <td>Jurusan</td>
+          <td>
+            <select>
+              <option>Pilih Jurusan</option>
+              <option value="Tarbiah"></option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>Tahun Lulus</td>
+          <td><select>
+            <option></option>
+            <option>2021</option>
+          </select></td>
+        </tr>
+
+      </body>
+      </html>
+
+
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<!-- link css -->
+<link rel="stylesheet" type="text/css" href="fontawose/css/all.min.css">
+<link rel="stylesheet" type="text/css" href="login.css">
+<link rel="stylesheet" type="text/css" href="css/boostrap.min.css">
+
+<body>
+	<!-- form login -->
+	<form class="box " action="#" method="post">
+		<h1>Login</h1>
+		<input type="Email" name="Email " placeholder="Email" >
+		<span class="simbol-input12">
+			<i class="fas fa-user-circle"></i>
+		</span>
+
+		<input type="password" name="password" placeholder="password">
+		<span class="simbol-input">
+			<i class="fa fa-lock" aria-hidden="true"></i>
+		</span>
+		
+		<h4 align="left" >Lupa Password?</h4>
+		<h5>*Klik buat akun jika anda belum memiliki akun</h5>
+		<input type="submit" name="submit" value="Buat Akun">
+		<input type="submit" name="submit" value="Masuk">
+	</form>
+</body>
+</html>
